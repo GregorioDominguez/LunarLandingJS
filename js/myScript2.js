dde6df77cb2dc323f3a18767d01d5c9575ad8261
GregorioDominguez <tupecitobueno@gmail.com>
2016-12-22 00:59:29 +0100

    Delete myScript2.js

--- js/myScript2.js
+++
@@ -1,7 +0,0 @@
-function alert2()
-{
-var idioma = document.getElementById(1).value;
-var indice = document.getElementById(1).selectedIndex;
-alert('Usted a elegido el idioma: ' + idioma + ' y su valor en \
-el indice es : ' + indice);
-}	

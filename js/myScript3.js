0b8d4d80ffe8c699494be5d41ba16657d181c014
GregorioDominguez <tupecitobueno@gmail.com>
2016-12-22 00:59:34 +0100

    Delete myScript3.js

--- js/myScript3.js
+++
@@ -1,9 +0,0 @@
-var Seleccionados = [];
-function alert2()
-{
-var idioma = document.getElementById(1).value;
-var indice = document.getElementById(1).selectedIndex;
-alert('Usted a elegido el idioma: ' + idioma + ' y su valor en el indice es : ' + indice);
-Seleccionados.push(idioma);
-console.log(Seleccionados);
-}	
